eac4936f46176c8dbd3131829caef55774ef2475
baelnog <1057456+baelnog@users.noreply.github.com>
2024-10-20 17:33:02 -0400

    more static analysis cleanup

--- ChecklistTracker/Controls/HintStoneControl.xaml.cs
+++ ChecklistTracker/Controls/HintStoneControl.xaml.cs
@@ -1,42 +1,28 @@
 using ChecklistTracker.Controls.Click;
+using ChecklistTracker.CoreUtils;
 using ChecklistTracker.ViewModel;
 using Microsoft.UI.Xaml.Controls;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
 
 namespace ChecklistTracker.Controls
 {
-    internal partial class HintStoneControl : UserControl
+    internal partial class HintStoneControl : UserControl, INotifyPropertyChanged
     {
-        internal LayoutParams _LayoutParams;
-        internal LayoutParams LayoutParams
-        {
-            get => _LayoutParams;
-            set
-            {
-                if (_LayoutParams != value)
-                {
-                    _LayoutParams = value;
-                    Margin = _LayoutParams.Padding;
-                }
-            }
-        }
 
-        private HintStoneViewModel _ViewModel;
-        internal HintStoneViewModel ViewModel
-        {
-            get => _ViewModel;
-            set
-            {
-                if (_ViewModel != value)
-                {
-                    _ViewModel = value;
-                    InitClickCallbacks();
-                }
-            }
-        }
+#pragma warning disable 67
+        public event PropertyChangedEventHandler? PropertyChanged;
+#pragma warning restore 67
+
+        internal LayoutParams? LayoutParams { get; set; }
+
+        internal HintStoneViewModel? ViewModel { get; set; }
 
         internal HintStoneControl()
         {
             InitializeComponent();
+
+            this.OnPropertyChanged(nameof(ViewModel), HandleViewModelChanged);
         }
 
         internal HintStoneControl(HintStoneViewModel viewModel, LayoutParams layout) : this()
@@ -45,8 +31,15 @@ namespace ChecklistTracker.Controls
             LayoutParams = layout;
         }
 
+        private void HandleViewModelChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            InitClickCallbacks();
+        }
+
         private void InitClickCallbacks()
         {
+            Contract.Assert(ViewModel != null);
+
             var callbacks = new ClickCallbacks();
             callbacks.OnClick = ViewModel.OnClick;
             callbacks.OnScroll = ViewModel.OnScroll;
