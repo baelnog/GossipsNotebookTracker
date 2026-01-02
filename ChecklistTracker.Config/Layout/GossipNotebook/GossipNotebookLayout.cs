fcfcfad7556f19ad04c6c0075c84001dc15aa067
baelnog <1057456+baelnog@users.noreply.github.com>
2026-01-02 13:42:40 -0500

    Use source generation for layout deserialization

--- ChecklistTracker.Config/Layout/GossipNotebook/GossipNotebookLayout.cs
+++ ChecklistTracker.Config/Layout/GossipNotebook/GossipNotebookLayout.cs
@@ -3,36 +3,52 @@ using ChecklistTracker.Config.Layout.GossipNotebook.Elements;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace ChecklistTracker.Layout.GossipNotebook
+namespace ChecklistTracker.Layout.GossipNotebook;
+
+public class GossipNotebookLayout
 {
-    public class GossipNotebookLayout
+    [JsonInclude]
+    public ChecklistTrackerConfig TrackerConfig = new ChecklistTrackerConfig();
+
+    [JsonInclude]
+    public StyleConfig Style = new StyleConfig();
+
+    [JsonInclude]
+    public IList<TrackerWindow> Windows = new List<TrackerWindow>();
+
+    public static GossipNotebookLayout ParseLayout(string layoutContent)
     {
-        [JsonInclude]
-        public ChecklistTrackerConfig TrackerConfig = new ChecklistTrackerConfig();
-
-        [JsonInclude]
-        public StyleConfig Style = new StyleConfig();
-
-        [JsonInclude]
-        public IList<TrackerWindow> Windows = new List<TrackerWindow>();
-
-        public static GossipNotebookLayout ParseLayout(string layoutContent)
-        {
-            return JsonSerializer.Deserialize<GossipNotebookLayout>(
-                layoutContent,
-                new JsonSerializerOptions
-                {
-                    AllowTrailingCommas = true,
-                    ReadCommentHandling = JsonCommentHandling.Skip,
-                    PropertyNameCaseInsensitive = true,
-                    Converters = {
-                        new JsonStringEnumConverter(JsonNamingPolicy.CamelCase),
-                        new ElementConverter(),
-                        new PaddingConverter(),
-                        new PositionConverter(),
-                        new SizeConverter(),
-                    }
-                }) ?? throw new Exception("Failed to parse layout file.");
-        }
+        return JsonSerializer.Deserialize<GossipNotebookLayout>(
+            layoutContent,
+            new JsonSerializerOptions
+            {
+                AllowTrailingCommas = true,
+                ReadCommentHandling = JsonCommentHandling.Skip,
+                PropertyNameCaseInsensitive = true,
+                TypeInfoResolver = GossipNotebokJsonContext.Default,
+                Converters = {
+                    new JsonStringEnumConverter(JsonNamingPolicy.CamelCase),
+                    new ElementConverter(),
+                    new PaddingConverter(),
+                    new PositionConverter(),
+                    new SizeConverter(),
+                }
+            }) ?? throw new Exception("Failed to parse layout file.");
     }
+}
+
+[JsonSerializable(typeof(GossipNotebookLayout))]
+[JsonSerializable(typeof(Element))]
+[JsonSerializable(typeof(ElementTable))]
+[JsonSerializable(typeof(HintTable))]
+[JsonSerializable(typeof(Label))]
+[JsonSerializable(typeof(ScreenshotElement))]
+[JsonSerializable(typeof(Padding))]
+[JsonSerializable(typeof(ConcretePadding))]
+[JsonSerializable(typeof(Position))]
+[JsonSerializable(typeof(ConcretePosition))]
+[JsonSerializable(typeof(Size))]
+[JsonSerializable(typeof(ConcreteSize))]
+internal partial class GossipNotebokJsonContext : JsonSerializerContext
+{ 
 }
